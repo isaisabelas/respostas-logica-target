474ccb765e4b25b67582fd7619e8aac7d6254a36
Isabela Silva <124411212+isaisabelas@users.noreply.github.com>
2024-03-07 23:59:12 -0300

    Create questao5.js

---
+++ questao5.js
@@ -0,0 +1,16 @@
+5) Escreva um programa que inverta os caracteres de um string.
+
+let word = prompt("Digite uma palavra:")
+
+function wordReverse (word) {
+  let newWord = []; //Criar um array para a nova palavra invertida 
+  
+  for (let i = 0; i < word.length; ++i) {
+    if (word [i] != '')
+        newWord.unshift(word[i])
+  } 
+  
+  return alert(`Sua nova palavra é: ${newWord}`);
+}
+
+wordReverse(word)
