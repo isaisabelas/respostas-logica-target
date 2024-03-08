8f6ea0d1a479d16bc8f303744e32d28c91c0312a
Isabela Silva <124411212+isaisabelas@users.noreply.github.com>
2024-03-07 22:01:44 -0300

    Add files via upload

---
+++ questao1.js
@@ -0,0 +1,15 @@
+//Para esta questão, trasformei o código dado para Javascript, pois é a linguagem que estou estudando momento. 
+//Nesse caso, pode-se resolver com um simples loop while 
+
+let indice = 13 
+let soma = 0
+let k = 0 
+
+while (k < indice) {
+  k++
+  soma += k 
+}
+
+console.log(soma)
+
+//Soma = 91
